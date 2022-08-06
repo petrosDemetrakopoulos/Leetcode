43dceb1066b29b44be4c232d7365e7b7c7ddf736
Petros <psdeme@hotmail.com>
2022-08-06 12:28:18 +0300

    added more solutions

---
+++ code/Python/2299-StrongPasswordCheckerII.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def strongPasswordCheckerII(self, password):
+        """
+        :type password: str
+        :rtype: bool
+        """
+        if len(password) < 8:
+            return False
+        lowercase = string.ascii_lowercase
+        uppercase = string.ascii_uppercase
+        digits = "0123456789"
+        special = "!@#$%^&*()-+"
+        has_lower = False
+        has_upper = False
+        has_digit = False
+        has_special = False
+        two_same_adjacent = False
+        for i in password:
+            if i in lowercase:
+                has_lower = True
+                break
+        for i in password:
+            if i in uppercase:
+                has_upper = True
+                break
+        for i in password:
+            if i in digits:
+                has_digit = True
+                break
+        for i in password:
+            if i in special:
+                has_special = True
+                break
+        for i in range(len(password)-1):
+            if password[i] == password[i+1]:
+                two_same_adjacent = True
+                break
+        return has_lower and has_upper and has_digit and has_special and not two_same_adjacent
+                
---
+++ code/Python/648-ReplaceWords.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def replaceWords(self, dictionary, sentence):
+        """
+        :type dictionary: List[str]
+        :type sentence: str
+        :rtype: str
+        """
+        l_sentence = sentence.split()
+        for i in range(len(l_sentence)):
+            for j in dictionary:
+                if l_sentence[i].startswith(j):
+                    l_sentence[i] = j
+        return " ".join(l_sentence)
