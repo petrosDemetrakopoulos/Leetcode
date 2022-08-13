29906d9bd95ce082d5d1458a9bfd51a1a03879c3
Petros <psdeme@hotmail.com>
2022-08-13 18:57:12 +0300

    added more solutions

--- code/Python/2273-FindResultantArrayAfterRemovingAnagrams.py
+++ code/Python/2273-FindResultantArrayAfterRemovingAnagrams.py
@@ -1,7 +1,4 @@
 class Solution(object):
-    def areAnagrams(self,str1,str2):
-        return sorted(list(str1)) == sorted(list(str2))
-    
     def removeAnagrams(self, words):
         """
         :type words: List[str]
@@ -9,7 +6,7 @@ class Solution(object):
         """
         i = len(words) -1
         while i > 0:
-            if self.areAnagrams(words[i],words[i-1]):
+            if sorted(list(words[i])) == sorted(list(words[i-1])):
                 del words[i]
                 i = len(words) -1
             else:
